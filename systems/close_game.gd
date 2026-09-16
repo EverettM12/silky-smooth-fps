bec06022953e3cd597284fcaa9bb341a350dddf7
EverettM12 <metivaeverett@gmail.com>
2026-09-16 19:34:16 -0400

    fixed close game

--- systems/close_game.gd
+++ systems/close_game.gd
@@ -1,6 +1,9 @@
 extends Node
 
-func close(message: String = "") -> void:
-	if !message.is_empty():
-		print(message)
+func close(message: String) -> void:
+	if message.is_empty():
+		print("Error, empty message though.")
+	
+	print_rich("[color=red][b]" + message + "[/b][/color]")
+	#await get_tree().process_frame
 	get_tree().quit()
