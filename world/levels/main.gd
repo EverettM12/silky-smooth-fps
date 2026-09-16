495c0b423da0f9551417728261b29c04d26364e8
EverettM12 <metivaeverett@gmail.com>
2026-09-16 19:28:00 -0400

    fix close game reference handling

--- world/levels/main.gd
+++ world/levels/main.gd
@@ -2,10 +2,14 @@ class_name Main
 extends Node3D
 
 @onready var player: Player = $Player
-@onready var start_pos: MeshInstance3D = $MeshInstance3D
+@onready var start_pos: MeshInstance3D = $"Start Pos"
 
 func _ready() -> void:
+	if !is_instance_valid(player):
+		CloseGame.close("Player missing!")
+		return
 	if !is_instance_valid(start_pos):
 		CloseGame.close("Start position missing!")
+		return
 
 	player.global_position = start_pos.global_position
